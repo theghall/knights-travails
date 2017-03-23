93993c7482463726a1631a08daf2739ed9bbb1a3
theghall <guategary@gmail.com>
2017-03-22 23:23:27 -0400

    Add visited to Vertex, refactor knight_move for visited, add print_path, add reset_visited, to_a to both Vertex and EndPoint, removed == from Vertex

--- knights_travails.rb
+++ knights_travails.rb
@@ -22,15 +22,26 @@ def build_edges(vertex)
     (0..1).each do |x|
       (0..1).each do |y|
         new_row = row + x_deltas[d_row][x]
+
         new_col = col + y_deltas[d_row][y]
-        vertex.end_points << Vertex::EndPoint.new(new_row, new_col, 1) if valid_pos(new_row,new_col)
+
+        vertex.end_points << Vertex::EndPoint.new(new_row, new_col, 1) \
+          if valid_pos(new_row, new_col)
       end
     end
   end
 end
 
+def reset_visited(board)
+  (0..7).each do |row|
+    (0..7).each do |col|
+      board[row][col].visited = false
+    end
+  end
+end
+
 def build_board_map
-  board = Array.new(8) {Array.new(8)}
+  board = Array.new(8) { Array.new(8) }
 
   (0..7).each do |row|
     (0..7).each do |col|
@@ -42,7 +53,30 @@ def build_board_map
   board
 end
 
+def print_path(root, end_vertex, board)
+  path = []
+
+  queue = [].push(end_vertex)
+
+  until queue.empty?
+    vertex = queue.shift
+
+    path.insert(0, vertex.to_a) if vertex.visited && !path.include?(vertex.to_a)
+
+    break if vertex == root
+
+    vertices = vertex.end_points
+
+    vertices.each { |vtx| queue.push(board[vtx.row][vtx.col]) }
+  end
+
+  path
+
+end
+
 def knight_move(start_pos, end_pos, board)
+  reset_visited(board)
+
   puts("Knight path for: #{start_pos} => #{end_pos}")
 
   root = board[start_pos[0]][start_pos[1]]
@@ -52,22 +86,26 @@ def knight_move(start_pos, end_pos, board)
   until queue.empty?
     vertex = queue.shift
 
-    found = vertex == end_pos
+    vertex.visited = true
+
+    found = vertex.to_a == end_pos
 
     break if found
 
     vertices = vertex.end_points
 
-    vertices.each do |vtx| 
-      queue.push(board[vtx.row][vtx.col])
-    end
-
+    vertices.each { |vtx| queue.push(board[vtx.row][vtx.col]) if !board[vtx.row][vtx.col].visited }
   end
 
-  found
+  print_path(root, vertex, board)
 end
 
 board = build_board_map
+
 p knight_move([0, 0], [1, 2], board)
+
 p knight_move([0, 0], [0, 4], board)
 
+p knight_move([0, 0], [3, 3], board)
+
+p knight_move([0, 0], [7, 7], board)
--- vertex.rb
+++ vertex.rb
@@ -3,23 +3,27 @@
 # 20170320	GH
 #
 module Vertex
+  # A node in a bi-directional tree
   class Vertex
-    attr_accessor :row, :col, :end_points
+    attr_accessor :row, :col, :visited, :end_points
 
     def initialize(row, col)
       @row = row
 
       @col = col
 
+      @visited = false
+
       @end_points = []
     end
 
-    def ==(pos)
-      row == pos[0] && col == pos[1]
+    def to_a
+      ary = []
+      ary << self.row << self.col
     end
-
   end
 
+  # An end-point for an edge, with weight of edge
   class EndPoint
     attr_accessor :row, :col, :weight
 
@@ -29,6 +33,11 @@ module Vertex
       @col = col
 
       @weight = weight
+    end
+
+    def to_a
+      ary = []
+      ary << self.row << self.col
     end
   end
 end
